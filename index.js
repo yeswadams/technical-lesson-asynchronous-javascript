23b61667844fbbc91c7186b7d793b334fadfa42b
yeswadams <yeswaadams73@gmail.com>
2026-04-20 15:49:20 +0300

    updated search functionality

--- index.js
+++ index.js
@@ -12,10 +12,26 @@
 //   });
 
 // This is with async/await
+let users = [];
+const searchInput = document.getElementById('search-bar');
+
+searchInput.addEventListener('input', (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    
+    // Filter the stored data
+    const filteredData = users.filter(item => 
+        item.name.toLowerCase().includes(searchTerm)
+    );
+    
+    displayUsers(filteredData);
+});
+
 async function fetchAndDisplayUsers() {
+    const loader = document.querySelector("svg");
+    loader.style.display = "block";
     try {
         const response = await fetch("https://jsonplaceholder.typicode.com/users");
-        const users = await response.json();
+        users = await response.json();
         displayUsers(users);
 
     } catch (error) {
@@ -24,12 +40,15 @@ async function fetchAndDisplayUsers() {
         errorMessage.textContent = "Failed to load user data. Please try again later.";
         userList.appendChild(errorMessage);
         console.error("Error fetching user data:", error);
+    } finally {
+        loader.style.display = "none";
     }
 }
 
 
 function displayUsers(users) {
   const userList = document.getElementById("user-list");
+  userList.innerHTML = "";
   users.forEach((user) => {
     const listItem = document.createElement("li");
     listItem.textContent = `${user.name} (${user.email})`;
